d11d999ee1306a878586dbb829db85753ce4a9e3
Geomedge <wieczorekr463@gmail.com>
2026-08-11 10:48:02 +0100

    Patch fixes

--- App.xaml.cs
+++ App.xaml.cs
@@ -28,10 +28,6 @@ namespace QR_Code_Generator
     {
         private Window? _window;
 
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             InitializeComponent();
